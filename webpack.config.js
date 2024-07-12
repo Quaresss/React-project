660a6a6a9c064f268bfc6f458a67b762f9c6cc41
Quaresss <roktarik777@gmail.com>
2024-07-12 20:14:27 +0400

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
                 test: /\.(ts|tsx)?$/,
                 use: "ts-loader",
                 exclude: /node_modules/,
+                
             },
             {
                 test: /\.s[ac]ss$/i,
